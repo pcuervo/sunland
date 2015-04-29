70180f3f88565e106d29fa22905eb1fc132def0f
ANKUSH <ankush7377@gmail.com>
2015-04-30 00:03:10 +0530

    date format improved

--- themes/sunland/index.php
+++ themes/sunland/index.php
@@ -145,11 +145,13 @@
 					if ( $query_events->have_posts() ) : while ( $query_events->have_posts() ) : $query_events->the_post();
 						$event_img_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
 						$meta_date = get_post_meta( $post->ID, '_dia_meta', true );
+						$newDate = date('jS F, Y', strtotime($meta_date));
+
 				?>
 						<div class="[ columna xmall-12 medium-4 ]">
 							<img src="<?php echo $event_img_url[0] ?>" class="[ image-responsive ] [ margin-bottom ]">
 							<h2 class="[ sub-title dark ] [ text-center ]"><?php the_title() ?></h2>
-							<p class="[ text-center ]"><?php echo $meta_date ?></p>
+							<p class="[ text-center ]"><?php echo $newDate ?></p>
 						</div>
 				<?php endwhile; endif; wp_reset_query(); ?>
 				
